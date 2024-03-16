0b8991c05cb07d55d45d782c540a776a3c9a5f77
Sumit Puri <sumitpuri006@gmail.com>
2024-03-16 20:51:45 +0530

    different way to get the stream of array and collection

---
+++ DSA/StreamFunction.java
@@ -0,0 +1,38 @@
+package DSA;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamFunction {
+          public static void main(String[] args) {
+                    // note that-> stream() is used to get stream from the list only;
+                    List<Integer> listVariable=List.of(3,5,6,3,8,4);
+                    listVariable.stream().map(sui ->sui +2).forEach(System.out::print);
+
+                    // note that -> Stream.of() is used to get the stream from an array of Wrapper class only 
+                    // which exclude int[] , to get the array of int[] we use these two ways given below;
+                    int sumit[]={7,35,5};
+                    IntStream.of(sumit).forEach(System.out::print);
+                    Arrays.stream(new int[]{6,9,4,2,4}).map(sum -> sum+2).forEach(System.out::println);
+                    
+                    // for regular wrapper arrays  
+                    List.of("sumit","vanya","puri").stream().forEach(System.out::print);
+
+          }
+}
+
+// import java.util.List;
+// import java.util.stream.Stream;
+
+// public class StreamFunction {
+//     public static void main(String[] args) {
+//         List<Integer> listVariable = List.of(3, 5, 6, 3, 8, 4);
+//         Stream.of(listVariable) // Create a stream directly from the list
+//                 .flatMap(List::stream) // FlatMap to convert List<Integer> to Stream<Integer>
+//                 .map(sui -> sui + 2)
+//                 .forEach(System.out::println);
+//     }
+// }
+ 
---
+++ DSA/intStream.java
@@ -0,0 +1,5 @@
+package DSA;
+
+public interface intStream {
+
+}
