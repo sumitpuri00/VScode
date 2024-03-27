07dd2051ca87c6afe89cd069843baa49b9c08281
Sumit Puri <sumitpuri006@gmail.com>
2024-03-27 16:24:36 +0530

    this is interface

---
+++ DSA/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+        public static void main(String[] args) {
+          System.out.println("Enter your data");
+          Integer name = System.console().readLine();
+          System.out.println("You entered: "+name);
+          
+        }
+
+}
---
+++ interfaceEx.java
@@ -0,0 +1,33 @@
+/**
+ * sumit
+ */
+interface sumit {
+
+          void num();
+
+
+}
+/**
+ * puri 
+ */
+class puri implements sumit {
+          public void print(){
+                    System.out.println("this is print");
+          }
+
+          @Override
+          public void num() {
+                    // TODO Auto-generated method stub
+                    System.out.println("this is interface method");
+          }
+          
+}
+public class interfaceEx {
+
+          public static void main(String[] args) {
+                    sumit suObj=new puri();
+                    // suObj.print(); this gives an error because only method which are 
+                    // inside the sumit interface are called ; because we are storing child refrence
+                    // inside the parent class; 
+          }
+}
